ea99613ae6e8db0a0ece6f27124799d72af078ac
Marcus <marcus.kariuki@student.moringaschool.com>
2023-01-12 06:12:55 +0300

    1.Pass down user data as props to the <Home/> component 2.Update <About/> component to add user bio 3.Create <links/> component

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,19 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
-  return (
-    <div id="about">
+function About(props) {
+  let propBio = "{prop.bio}";
+
+  if (!propBio){
+    return null;
+  }
+    return (
+      <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github = {props.links.github} linkedin = {props.links.linkedin} />
+
     </div>
   );
 }
